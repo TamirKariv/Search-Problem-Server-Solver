5c8d9289a8cfc153d9b915b8821b4b439e5d3d09
Tamir Kariv <tamir.kariv@gmail.com>
2020-12-27 18:01:51 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/untitled19.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tamir/Desktop/git/advanced prog 2/src/AStar.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/AStar.cpp.o"
+  "/home/tamir/Desktop/git/advanced prog 2/src/BFS.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/BFS.cpp.o"
+  "/home/tamir/Desktop/git/advanced prog 2/src/BestFirstSearch.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/BestFirstSearch.cpp.o"
+  "/home/tamir/Desktop/git/advanced prog 2/src/DFS.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/DFS.cpp.o"
+  "/home/tamir/Desktop/git/advanced prog 2/src/DataReaderClient.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/DataReaderClient.cpp.o"
+  "/home/tamir/Desktop/git/advanced prog 2/src/DataReaderServer.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/DataReaderServer.cpp.o"
+  "/home/tamir/Desktop/git/advanced prog 2/src/FileCacheManager.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/FileCacheManager.cpp.o"
+  "/home/tamir/Desktop/git/advanced prog 2/src/MatrixSearchable.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/MatrixSearchable.cpp.o"
+  "/home/tamir/Desktop/git/advanced prog 2/src/MySerialServer.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/MySerialServer.cpp.o"
+  "/home/tamir/Desktop/git/advanced prog 2/src/MySolver.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/MySolver.cpp.o"
+  "/home/tamir/Desktop/git/advanced prog 2/src/MyTestClientHandler.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/MyTestClientHandler.cpp.o"
+  "/home/tamir/Desktop/git/advanced prog 2/src/Vertex.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/Vertex.cpp.o"
+  "/home/tamir/Desktop/git/advanced prog 2/src/main.cpp" "/home/tamir/Desktop/git/advanced prog 2/src/cmake-build-debug/CMakeFiles/untitled19.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled19.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled19.dir/AStar.cpp.o"
+  "CMakeFiles/untitled19.dir/BFS.cpp.o"
+  "CMakeFiles/untitled19.dir/BestFirstSearch.cpp.o"
+  "CMakeFiles/untitled19.dir/DFS.cpp.o"
+  "CMakeFiles/untitled19.dir/DataReaderClient.cpp.o"
+  "CMakeFiles/untitled19.dir/DataReaderServer.cpp.o"
+  "CMakeFiles/untitled19.dir/FileCacheManager.cpp.o"
+  "CMakeFiles/untitled19.dir/MatrixSearchable.cpp.o"
+  "CMakeFiles/untitled19.dir/MySerialServer.cpp.o"
+  "CMakeFiles/untitled19.dir/MySolver.cpp.o"
+  "CMakeFiles/untitled19.dir/MyTestClientHandler.cpp.o"
+  "CMakeFiles/untitled19.dir/Vertex.cpp.o"
+  "CMakeFiles/untitled19.dir/main.cpp.o"
+  "untitled19"
+  "untitled19.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled19.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
